e1b1a086d7fd89bfe10dde800d59f9be18f743b9
Mahdi Nedaee <53142654+mhdned@users.noreply.github.com>
2026-08-28 12:22:43 +0000

    REMOVE misspelled change_detection/__inti__.py leftover

--- fezrs/tools/change_detection/__inti__.py
+++
@@ -1,5 +0,0 @@
-from .burn_calculator import BurnCalculator
-from .indices_calculator import IndicesCalculator
-from .magdir_calculator import MagDirCalculator
-from .subdiv_calculator import SubDivCalculator
-from .time_calculator import TimeCalculator
